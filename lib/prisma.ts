bcb481a08bf1db4b4dc7fa76bf15fdcf3b1f9375
Gerasimos Alven <alvengabriel@gmail.com>
2026-06-07 21:16:45 +0700

    feat(Auth-iteration4

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,6 +2,12 @@ import { PrismaClient } from "../app/generated/prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = globalForPrisma.prisma ?? new PrismaClient();
+export const prisma =
+  globalForPrisma.prisma ??
+  new PrismaClient({
+    datasourceUrl: process.env.DATABASE_URL,
+  } as ConstructorParameters<typeof PrismaClient>[0]);
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+  globalForPrisma.prisma = prisma;
+}
